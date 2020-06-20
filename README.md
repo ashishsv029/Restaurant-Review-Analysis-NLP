6cdf3236bec7591788760119265179dc2c79c09f
Nag Ashish S V <50989117+ashishsv029@users.noreply.github.com>
2020-06-20 22:20:57 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Restaurant-Review-Analysis-NLP
-This is an NLP application which classifies the restaurant reviews as positive or negitive using BOW(Bag Of Words)  and Random forest classifier.
+Restaurant Review Analysis using NLP
+   -We used Random forest classifier to classify whether a review is positive or negitive
---
+++ app.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 20 19:13:46 2020
+
+@author: LRG
+"""
+
+import numpy as np
+from flask import Flask,request,jsonify,render_template
+import pickle
+import re
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+app=Flask(__name__)
+model=pickle.load(open('restaurant_model.pkl','rb'))
+
+cv=pickle.load(open('transform.pkl','rb'))
+
+@app.route('/predictor',methods=['POST'])
+def predictor():
+    st_array=list(request.get_json(force=True).values())[0]
+    corpus=[]
+    for st in st_array:
+        review=re.sub('[^a-zA-z]',' ',st)
+        review=review.lower()
+        review=review.split()
+        ps=PorterStemmer()
+        review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+        review=' '.join(review)
+        corpus.append(review)
+    trail_x=cv.transform(corpus).toarray()
+    trail_pred=model.predict(trail_x)
+    lst=[]
+    for i in trail_pred:
+        lst.append(int(i))
+    print(lst)
+    return jsonify(outputs=lst)
+    #return(trail_pred)
+
+
+#predictor(["so nice","food is bad"])
+    
+if __name__=="__main__":
+    app.run(debug=False)
