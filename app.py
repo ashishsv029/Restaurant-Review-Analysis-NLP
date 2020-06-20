eae623ff32b712d870ec81dcbeba19bb5be6b97e
Nag Ashish S V <50989117+ashishsv029@users.noreply.github.com>
2020-06-21 01:40:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,19 @@ import numpy as np
 from flask import Flask,request,jsonify,render_template
 import pickle
 import re
-import nltk
+#import nltk
 
 #from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
+#from nltk.stem.porter import PorterStemmer
 
 app=Flask(__name__)
 model=pickle.load(open('restaurant_model.pkl','rb'))
 
 cv=pickle.load(open('transform.pkl','rb'))
+
+@app.route('/',methods=['GET'])
+def home():
+    return "<h1>NLP-APP</h1>"
 
 @app.route('/predictor',methods=['POST'])
 def predictor():
@@ -27,8 +31,8 @@ def predictor():
         review=re.sub('[^a-zA-z]',' ',st)
         review=review.lower()
         review=review.split()
-        ps=PorterStemmer()
-        review = [ps.stem(word) for word in review if not word in ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]]
+        #ps=PorterStemmer()
+        review = [word for word in review if not word in ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]]
         review=' '.join(review)
         corpus.append(review)
     trail_x=cv.transform(corpus).toarray()
